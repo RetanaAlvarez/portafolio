3a8471cb19c35b57d1991057fdbca735e969e781
RetanaAlvarez <chepe_jm10@hotmail.com>
2026-03-28 20:38:47 -0600

    fix imagenes

--- src/assets/icons.jsx
+++ src/assets/icons.jsx
@@ -1,12 +1,14 @@
+import { DiVisualstudio } from "react-icons/di";
 import { FaJava, FaReact, FaGitAlt, FaLinux} from "react-icons/fa";
-import { SiSpringboot, SiOracle, SiPostman } from "react-icons/si";
+import { SiSpringboot, SiOracle, SiPostman, SiSwagger } from "react-icons/si";
 
 export const techIcons = {
-  Java: () => <FaJava color="#fca311" size={40} />,
-  "Spring Boot": () => <SiSpringboot color="#00e676" size={40} />,
-  Oracle: () => <SiOracle color="#ff6b6b" size={40} />,
-  React: () => <FaReact color="#00e5ff" size={40} />,
-  Git: () => <FaGitAlt color="#ff8c00" size={40} />,
-  Linux: () => <FaLinux color="#f6c400" size={40} />,
-  Postman: () => <SiPostman color="#ff8c00" size={40} />
+  java: () => <FaJava color="#fca311" size={40} />,
+  "spring boot": () => <SiSpringboot color="#00e676" size={40} />,
+  oracle: () => <SiOracle color="#ff6b6b" size={40} />,
+  react: () => <FaReact color="#00e5ff" size={40} />,
+  git: () => <FaGitAlt color="#ff8c00" size={40} />,
+  linux: () => <FaLinux color="#f6c400" size={40} />,
+  postman: () => <SiPostman color="#ff8c00" size={40} />,
+  swagger:() => <SiSwagger color="#00e676" size={40}/>
 };
