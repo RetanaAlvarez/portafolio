5fa1a489d15485a15fb91ce6d1a97209565d2bc9
RetanaAlvarez <chepe_jm10@hotmail.com>
2026-03-28 20:41:13 -0600

    fix estructura y diseno

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,13 @@
 import texto from '../../data/Texto.json';
 
+import style from "./Footer.module.css"
+
 function Footer() {
   return (
-    <footer>
-        <p>{texto.copyright} {texto.autor} {texto.rol}</p>
+    <footer className={style.footer}>
+      <p>{texto.copyright} {texto.autor} — {texto.rol}</p>
     </footer>
-  )
+  );
 }
 
 export default Footer;
