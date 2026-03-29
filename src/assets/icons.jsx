a2202588eb3e876a07104c493bc190b5b7199046
RetanaAlvarez <chepe_jm10@hotmail.com>
2026-03-28 20:56:30 -0600

    fix logo oracle

--- src/assets/icons.jsx
+++ src/assets/icons.jsx
@@ -1,11 +1,11 @@
 import { DiVisualstudio } from "react-icons/di";
-import { FaJava, FaReact, FaGitAlt, FaLinux} from "react-icons/fa";
-import { SiSpringboot, SiOracle, SiPostman, SiSwagger } from "react-icons/si";
+import { FaJava, FaReact, FaGitAlt, FaLinux,FaDatabase} from "react-icons/fa";
+import { SiSpringboot, SiPostman, SiSwagger } from "react-icons/si";
 
 export const techIcons = {
   java: () => <FaJava color="#fca311" size={40} />,
   "spring boot": () => <SiSpringboot color="#00e676" size={40} />,
-  oracle: () => <SiOracle color="#ff6b6b" size={40} />,
+  oracle: () => <FaDatabase color="#ff6b6b" size={40} />,
   react: () => <FaReact color="#00e5ff" size={40} />,
   git: () => <FaGitAlt color="#ff8c00" size={40} />,
   linux: () => <FaLinux color="#f6c400" size={40} />,
